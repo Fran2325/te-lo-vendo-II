0622fc67344a3d874e3cccbfac4d0f20271577cc
Fran <francismartinez2325@gmail.com>
2021-09-10 21:27:54 -0300

    Se elimino elimino una carpeta

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 
+
 let d = new Date();
 let f = ('Fecha: '+d.getDate()+'<br>Dia de la semana: '+d.getDay()+'<br>Mes (0 al 11): '+d.getMonth()+'<br>Año: '+d.getFullYear()+'<br>Hora: '+d.getHours()+'<br>Hora UTC: '+d.getUTCHours()+'<br>Minutos: '+d.getMinutes()+'<br>Segundos: '+d.getSeconds());
 
@@ -7,3 +8,18 @@ document.getElementById("datos").innerHTML=f;
 
 let r = new Date();
 document.getElementById("Fecha").innerHTML=r;
+
+function addZero(i) {
+    if (i < 10) {
+        i = "0" + i;
+    }
+    return i;
+}
+function myFunction() {
+    var d = new Date();
+    var x = document.getElementById("demo");
+    var h = addZero(d.getHours());
+    var m = addZero(d.getMinutes());
+    var s = addZero(d.getSeconds());
+    x.innerHTML = h + ":" + m + ":" + s;
+}
